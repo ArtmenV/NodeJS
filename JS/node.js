02eb409e471f4ebc65514977c90dea1834eddaba
Artmen_V <viliutich.artem@gmail.com>
2019-01-02 11:53:41 +0300

    First commit

---
+++ JS/node.js
@@ -0,0 +1,13 @@
+// Math.floor
+// Rounds down: 3.1 becomes 3, and -1.1 becomes -2.
+// Math.ceil
+// Rounds up: 3.1 becomes 4, and -1.1 becomes -1.
+// Math.round
+// // Rounds to the nearest integer: 3.1 becomes 3, 3.6 becomes 4 and -1.1 becomes -1.
+
+// let a = +prompt("The first number?", "");
+// let b = +prompt("The second number?", "");
+
+// alert( a + b );
+
+alert( 6.35.toFixed(10) );
